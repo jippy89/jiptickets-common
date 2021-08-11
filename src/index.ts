e0521ee6a3f62f60b49b0462377eadab6fb5c1f1
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-11 15:34:37 +0700

    feat(index.ts): import files existing files needed by other services

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-interface Color {
-  red: number,
-  green: number,
-  blue: number
-}
+// There are two ways you can let other developers access your packages
+// --- Option 1. Leaving as it is
+// And developer can access your packages by `import { ConflictError } from '@jiptickets/common/errors/conflict-error'
+// The cons of this is that the dev have to know which path does the object located
+// --- Option 2. Put everything in `index.ts` (the main file which would be later known as `index.js`)
+// Developer will be able to access using `import { ConflictError } from '@jiptickets/common`
+// We will go with option 2!
 
-const color:Color = {
-  red: 10,
-  green: 10,
-  blue: 10
-}
+// Errors
+export * from './errors/conflict-error'
+export * from './errors/database-connection-error'
+export * from './errors/not-authorized-error'
+export * from './errors/not-found-error'
+export * from './errors/request-validation-error'
 
-console.log(color)
-
-export default color
+// Middlewares
+export * from './middlewares/current-user'
+export * from './middlewares/error-handler'
+export * from './middlewares/require-auth'
+export * from './middlewares/validate-request'
