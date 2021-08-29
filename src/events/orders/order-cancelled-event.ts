f21322327bb7f69850040226a887275b5f8ac0fa
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-29 13:54:14 +0700

    feat(order-cancelled-event.ts, order-created-event.ts): add event

---
+++ src/events/orders/order-cancelled-event.ts
@@ -0,0 +1,20 @@
+import { Event } from "../Event";
+import { Subjects } from "../subjects";
+import { OrderStatus } from "../types/order-status";
+
+// In case of choosing how the `data` should transfer
+// it should be really up to you
+// Do you want to:
+// 1. Share the maximum amount of information as much as possible?
+// Or 2. Share the minimum info?
+// FYI, if in the future you the other services need a new info
+// You can always go back here and update the 'data'.
+export interface OrderCancelledEvent extends Event {
+  subject: Subjects.OrderCancelled
+  data: {
+    id: string
+    ticket: {
+      id: string
+    }
+  }
+}
