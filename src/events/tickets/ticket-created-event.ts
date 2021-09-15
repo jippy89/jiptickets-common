2edb4e27a9de9bff0acced0274bcce11947ba182
Jip Irfandy <irfandyjip98@gmail.com>
2021-09-15 23:19:55 +0700

    refactor(events): add `version` to event `data`
    This is because we are using OCC, this was changed on "Section 19 no 390, Including Version in Events"

--- src/events/tickets/ticket-created-event.ts
+++ src/events/tickets/ticket-created-event.ts
@@ -5,6 +5,7 @@ export interface TicketCreatedEvent extends Event {
   subject: Subjects.TicketCreated
   data: {
     id: string,
+    version: number
     title: string,
     price: number,
     userId: string
